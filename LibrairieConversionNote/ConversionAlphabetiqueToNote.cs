0430c60a2db472899d0ecbe4ffc0e31db89b9190
cesarsmallgrasset <105759347+cesarsmallgrasset@users.noreply.github.com>
2022-06-15 22:03:48 -0400

    second commit

---
+++ LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,36 @@
+﻿namespace LibrairieConversionNote
+{
+    
+        public class ConversionAlphabetiqueToNote
+        {
+
+
+
+            public string Transformer(string input)
+            {
+
+                List<string> NotesAnglais = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+                List<string> notes = new List<string>() { "LA", "SI", "DO", "RE", "MI", "FA", "SOL" };
+
+
+                for (int i = 0; i < NotesAnglais.Count; i++)
+                {
+                    string position = NotesAnglais[i];
+
+                    if (position == input)
+                    {
+
+                        return notes[i];
+
+                    }
+
+                }
+                return "option invalide";
+
+            }
+
+
+
+        }
+
+}
--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -1,10 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LibrairieConversionNote;
 
-while (true) {
+
+bool game = true;
+
+while (true)
+{
     Console.WriteLine("Quelle type de conversion voulez-vous faire ?");
-  
+
+    while (game == true)
+    {
+        Console.WriteLine("Note a convertir:");
+
+        string input = Console.ReadLine().ToUpper();
 
 
+        string output = new ConversionAlphabetiqueToNote().Transformer(input);
 
 
-
+        Console.WriteLine(output);
+    }
+}
